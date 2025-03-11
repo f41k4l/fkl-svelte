944f79bc15413086cc99ab8b8a9528059d320a40
f41k4l <clement.lerch@provectio.fr>
2025-03-11 17:28:16 +0100

    correct event

--- useClickOutside/index.js
+++ useClickOutside/index.js
@@ -1,7 +1,7 @@
 export function clickOutside(element, callbackFunction) {
   function onClick(event) {
     if (!element.contains(event.target)) {
-      callbackFunction(e);
+      callbackFunction(event);
     }
   }
 
