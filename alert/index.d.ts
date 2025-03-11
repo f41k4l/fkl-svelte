c5a6c0c3aa660e44fd09aa838f6ceda050f07ccc
f41k4l <clement.lerch@provectio.fr>
2025-03-11 15:19:28 +0100

    adding delcaration module

---
+++ alert/index.d.ts
@@ -0,0 +1,10 @@
+declare module "./Alert.svelte" {
+  import type { ComponentPropsWithoutRef } from "svelte";
+  export interface AlertProps extends ComponentPropsWithoutRef<Alert> {
+    text: string;
+    description?: string;
+    type?: "info" | "success" | "warning" | "error";
+    disableIcon?: boolean;
+    uppercase?: boolean;
+  }
+}
